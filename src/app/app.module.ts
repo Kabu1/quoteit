60ccc2bd792d037582cdce2899dceed7e81303cd
kabu1 <kkabubenson@gmail.com>
2020-08-23 17:57:26 -0400

    Add css styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './details/details.component';
 import { FormComponent } from './form/form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DetailsComponent,
-    FormComponent
+    FormComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
