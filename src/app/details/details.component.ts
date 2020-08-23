a2105f82135d61ca411c6beb0bfa78e6d0a474f7
kabu1 <kkabubenson@gmail.com>
2020-08-23 17:53:47 -0400

    first commit

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { quote } from "../quote"
+
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+  @Input() quote:quote
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
